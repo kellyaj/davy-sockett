9b55eedd0b006fecbaf7b49560f17116055f041a
Andrew Kelly <kellyaj@gmail.com>
2013-04-23 16:47:27 -0500

    printing to proper outputstream now

--- src/davy_sockett/core.clj
+++ src/davy_sockett/core.clj
@@ -1,19 +1,19 @@
 (ns davy-sockett.core)
 
-(import '[java.net InetAddress InetSocketAddress])
+(import '[java.net InetAddress InetSocketAddress ServerSocket])
 (import '[java.nio.channels ServerSocketChannel])
-(import '[java.io OutputStreamWriter])
+(import '[java.io BufferedReader InputStreamReader PrintWriter])
 
 
-(defn create-server [port]
-  (let [x (->
-            (ServerSocketChannel/open)
-            (.bind (InetSocketAddress. "localhost" 5000)))]
-    (-> (.socket x) .accept)))
+(defn create-server []
+  (let [davy ( ServerSocket.)]
+    (-> davy (.bind (InetSocketAddress. "localhost" 5000)))
+    (binding [*out* (PrintWriter. (.getOutputStream (.accept davy)))]
+      (println "hello world"))))
 
 
 (defn -main []
-  (create-server 5000))
+  (create-server))
 
 
 ;(println "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\r\nHello World")))
