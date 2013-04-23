49b11606cf3caa14bd5d059e928492cd8c1db5b7
Andrew Kelly <kellyaj@gmail.com>
2013-04-23 15:31:32 -0500

    seems to be accepting connections, sort of

--- src/davy_sockett/core.clj
+++ src/davy_sockett/core.clj
@@ -2,15 +2,18 @@
 
 (import '[java.net InetAddress InetSocketAddress])
 (import '[java.nio.channels ServerSocketChannel])
+(import '[java.io OutputStreamWriter])
 
 
 (defn create-server [port]
   (let [x (->
             (ServerSocketChannel/open)
-            (.configureBlocking false)
-            (.bind (InetSocketAddress. "localhost" port)))]
-    ))
+            (.bind (InetSocketAddress. "localhost" 5000)))]
+    (-> (.socket x) .accept)))
 
 
 (defn -main []
   (create-server 5000))
+
+
+;(println "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\r\nHello World")))
