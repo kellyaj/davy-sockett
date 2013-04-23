53c94cf58338a42a26528de7c2f8aa2ce6e490fd
Andrew Kelly <kellyaj@gmail.com>
2013-04-23 12:12:55 -0500

    first commit.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject davy-sockett "0.1.0-SNAPSHOT"
+  :description "Exploring the ServerSocket frontier"
+  :url "http://akells.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :plugins [[speclj "2.5.0"]]
+  :test-paths ["spec"]
+  :main davy_sockett.core)
---
+++ spec/davy_sockett/core_spec.clj
@@ -0,0 +1,7 @@
+(ns davy-sockett.core-spec
+  (:require [speclj.core :refer :all]
+            [davy-sockett.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
