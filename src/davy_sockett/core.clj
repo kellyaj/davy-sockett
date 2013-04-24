49ccf82017a182fb50b0d5faf2cfc54528c4c3ec
Andrew Kelly <kellyaj@gmail.com>
2013-04-24 11:20:09 -0500

    first steps to spinning off a new thread

--- src/davy_sockett/core.clj
+++ src/davy_sockett/core.clj
@@ -4,14 +4,15 @@
 (import '[java.nio.channels ServerSocketChannel])
 (import '[java.io BufferedReader InputStreamReader PrintWriter])
 
+(defn spinoff [new-socket]
+  (binding [*out* (PrintWriter. (.getOutputStream new-socket))
+            *in* (BufferedReader. (InputStreamReader. (.getInputStream new-socket)))]
+    (println (read-line))))
 
 (defn create-server []
   (let [davy (ServerSocket.)]
     (-> davy (.bind (InetSocketAddress. "localhost" 5000)))
-    (let [socket (.accept davy)]
-    (binding [*out* (PrintWriter. (.getOutputStream socket))
-              *in* (BufferedReader. (InputStreamReader. (.getInputStream socket)))]
-      (println (read-line))))))
+    (spinoff (.accept davy))))
 
 
 (defn -main []
